2a120a02360e9300747dded6cad24b4fe23818cb
risan <risanbagja@gmail.com>
2017-02-11 22:55:57 +0100

    :sparkles: Try to remove certificate from trusted list and keychain.

--- removesite.sh
+++ removesite.sh
@@ -15,6 +15,7 @@ LOGS_DIR="$BALET_DIR/logs"
 SERVERS_DIR="$BALET_DIR/servers"
 SSL_DIR="$BALET_DIR/ssl"
 HOSTS_FILE="/etc/hosts"
+SYSTEM_KEYCHAIN_PATH="/Library/Keychains/System.keychain"
 
 # Make sure that the website name is set.
 if [ -z $WEBSITE_NAME ]; then
@@ -26,6 +27,7 @@ fi
 WEBSITE_CONFIG_FILE="$SERVERS_DIR/$WEBSITE_NAME"
 WEBSITE_ERROR_LOG_FILE="$LOGS_DIR/$WEBSITE_NAME-error.log"
 WEBSITE_SSL_DIR="$BALET_DIR/ssl/$WEBSITE_NAME"
+WEBSITE_SSL_CERTIFICATE_FILE="$WEBSITE_SSL_DIR/certificate.crt"
 
 # Check if the webiste configuration file is not exist.
 if [ ! -f $WEBSITE_CONFIG_FILE ]; then
@@ -33,18 +35,36 @@ if [ ! -f $WEBSITE_CONFIG_FILE ]; then
   exit 1
 fi
 
-# Delete the configuration file, log, and certificate directory.
-echo "${CYAN}Removing website configuration file for: $WEBSITE_NAME...${NC}"
+# Welcome message.
+echo "${CYAN}Removing $WEBSITE_NAME website...${NC}"
+
+# Delete the configuration file.
+echo "${CYAN}Removing website configuration...${NC}"
 rm "$WEBSITE_CONFIG_FILE"
+
+# Delete the error log file.
+echo "${CYAN}Removing website error log file...${NC}"
 rm "$WEBSITE_ERROR_LOG_FILE"
+
+# Remove from the trusted list.
+echo "${CYAN}Try removing the SSL certificate from the trusted list...${NC}"
+security remove-trusted-cert -d "$WEBSITE_SSL_CERTIFICATE_FILE"
+
+# Remove certificate from keychain.
+echo "${CYAN}Try removing the SSL certificate from the keychain...${NC}"
+security delete-certificate -c "$WEBSITE_NAME" "$SYSTEM_KEYCHAIN_PATH"
+
+# Remove certificate directory.
+echo "${CYAN}Removing website SSL directory...${NC}"
 rm -Rf "$WEBSITE_SSL_DIR"
 
 # Remove the domain from the hosts file.
-echo "${CYAN}Updating hosts file...${NC}"
+echo "${CYAN}Updating the hosts file...${NC}"
 sed -i '' "/127.0.0.1 $WEBSITE_NAME/d" $HOSTS_FILE
 
 # Reload nginx.
-echo "${CYAN}Restarting Nginx server...${NC}"
+echo "${CYAN}Restarting the Nginx server...${NC}"
 nginx -s reload
 
+# Goodbye message.
 echo "${GREEN}The $WEBSITE_NAME website is removed \xE2\x9C\x94${NC}"
